0c9fa23e095d2a78dbb5dba010a10f0d8cafd425
BunnyHatcher <martinengelhaus@gmx.de>
2022-09-30 14:10:25 +0200

    Implemented Character Movement

---
+++ FPS Audio Example/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    CharacterController _characterController;
+
+
+    private void Awake()
+    {
+        //create a controller
+        _characterController = GetComponent<CharacterController>();
+        
+    }
+
+
+    private void Update()
+    {
+        //Get Movement Input
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        //calculate direction with the help of both input values
+        Vector3 direction = transform.right * horizontalInput + transform.forward * verticalInput;
+
+        // calculate movement by multiplying direction with speed (and deltatime to make movement frame-rate independent) 
+        Vector3 move = direction * _speed * Time.deltaTime;
+
+        //Create Movement function
+
+        _characterController.Move(move);
+
+
+    }
+}
