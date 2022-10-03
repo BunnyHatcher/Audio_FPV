3f1344a898c9b3ddd2d6c26d4bf5a03a22c7efaa
BunnyHatcher <martinengelhaus@gmx.de>
2022-10-03 23:56:38 +0200

    unsuccesfull attempt to implement footstep sounds

--- FPS Audio Example/Assets/Scripts/PlayerMovement.cs
+++ FPS Audio Example/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed;
+    [SerializeField] private float _normalSpeed;
+
+    [SerializeField] private float _sprintSpeed;
+
+    private float _speed;
 
     // instead of using rigidbody, we use a character controller
-    CharacterController _characterController; 
+    CharacterController _characterController;
+
+    AudioSource _audioSource;
 
 
     private void Awake()
     {
         //get the controller
         _characterController = GetComponent<CharacterController>();
+
+        //get AudioSource
+        _audioSource = gameObject.GetComponent<AudioSource>();
         
     }
 
 
+    
     private void Update()
     {
         //Get Movement Input
@@ -27,11 +37,27 @@ public class PlayerMovement : MonoBehaviour
         //calculate direction with the help of both input values
         Vector3 direction = transform.right * horizontalInput + transform.forward * verticalInput;
 
+        //Check if sprinting or not
+
+        if (Input.GetButtonDown("Sprint"))
+        {
+            _speed = _sprintSpeed;
+        }
+
+        else
+        {
+            _speed = _normalSpeed;
+        }
+
         // calculate movement by multiplying direction with speed (and deltatime to make movement frame-rate independent) 
         Vector3 move = direction * _speed * Time.deltaTime;
 
         //Create Movement function
         _characterController.Move(move);
+
+        //set pitch to player speed
+        _audioSource.pitch = _characterController.velocity.magnitude / _normalSpeed;
+
 
 
     }
