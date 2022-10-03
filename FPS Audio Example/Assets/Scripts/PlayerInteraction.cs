6b079031255a7dfd6bcf06fa311affddc3e34fbe
Martin Engelhaus <martinengelhaus@gmx.de>
2022-10-03 12:36:27 +0200

    changed script to change color when object interactable + audio mixer and groups, set up loudspeaker

--- FPS Audio Example/Assets/Scripts/PlayerInteraction.cs
+++ FPS Audio Example/Assets/Scripts/PlayerInteraction.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    [SerializeField] private Color _colorReticuleUse;
+    [SerializeField] private Color _colorReticuleIdle;
+
     [SerializeField] private float _maxDistance = 100f;
 
     [SerializeField] private IUsable _target;
@@ -27,7 +31,7 @@ public class PlayerInteraction : MonoBehaviour
 
         UseTarget();
 
-        ChangeCrossHairState();
+        //ChangeCrossHairState(Color.red);
     }
 
     private void FindTarget()
@@ -39,23 +43,28 @@ public class PlayerInteraction : MonoBehaviour
         if (hit.collider == null || hit.collider.gameObject.TryGetComponent(out IUsable _interface) == false )
         {
             _target = null;
+            ChangeCrossHairState(_colorReticuleIdle);
         }
 
         else
         {
             _target = _interface;
+            ChangeCrossHairState(_colorReticuleUse);
 
-            
+
         }
     }
 
     private void UseTarget()
     {
-        if (Input.GetButtonDown("Fire1")) _target.Use(); //if you put a single instruction into the same line, noi need to put brackets
+        if (Input.GetButtonDown("Fire1")) _target.Use(); //if you put a single instruction into the same line, no need to put brackets
     }
 
-    private void ChangeCrossHairState()
+    public void ChangeCrossHairState(Color color)
     {
+        GameObject _crosshair = GameObject.Find("Crosshair");
+        Image image = _crosshair.GetComponent<Image>();
+        image.color = color;
 
     }
 
--- FPS Audio Example/Assets/Test.cs
+++ FPS Audio Example/Assets/Test.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Test : MonoBehaviour, IUsable
 {
+    PlayerInteraction playerInteraction;
+
+    private void Start()
+    {
+        playerInteraction = FindObjectOfType<PlayerInteraction>();
+            
+    }
+
     public void Use()
     {
         Debug.Log("Touchdown!");
+        playerInteraction.ChangeCrossHairState(Color.magenta);
     }
 }
